f201ea5975142fbf0b0ac7dec395ea4e35af6d28
Iain05 <58150364+Iain05@users.noreply.github.com>
2020-04-19 13:06:12 -0700

    Create J1.java
    added the stuff and took out the trash

---
+++ 2018/J1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+  	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		int num_1 = input.nextInt();
+		int num_2 = input.nextInt();
+		int num_3 = input.nextInt();
+		int num_4 = input.nextInt();
+		input.close();
+
+		if ((num_1 == 8 || num_1 == 9) && (num_2 == num_3) && (num_4 == 8 || 	num_4 == 9)) {
+			System.out.println("ignore");
+		} else {
+			System.out.println("answer");
+		} 
+	}
+}
