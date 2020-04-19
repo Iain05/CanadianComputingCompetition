f089b6083332359287e5c561616beaeeea139948
Iain05 <58150364+Iain05@users.noreply.github.com>
2020-04-19 13:07:09 -0700

    Update J1.java
    fix the ugly tab sizes

--- 2018/J1.java
+++ 2018/J1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Main {
-  	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
 		int num_1 = input.nextInt();
