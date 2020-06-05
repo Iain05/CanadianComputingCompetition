2afacd0158ad8edfa1f7b3457e4af926f10cbd2f
Iain05 <58150364+Iain05@users.noreply.github.com>
2020-06-05 10:09:00 -0700

    ezpz

---
+++ 2020/J1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+
+	  int S = input.nextInt();
+	  int M = input.nextInt();
+	  int L = input.nextInt();
+	  input.close();
+
+	  if (1 * S + 2 * M + 3 * L >= 10) {
+		  System.out.println("Happy");
+	  } else {
+		  System.out.println("Sad");
+	  }
+  }
+}
