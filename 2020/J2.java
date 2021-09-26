9ff026bc1ed58036a08265489f06b28e43219854
Iain05 <58150364+Iain05@users.noreply.github.com>
2021-09-25 21:33:59 -0700

    Create J2.java

---
+++ 2020/J2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+
+        int p = input.nextInt();
+        int n = input.nextInt();
+        int r = input.nextInt();
+		input.close();
+
+        int day = 0;
+		int totalInfected = 0;
+
+        while (totalInfected <= p) {
+			totalInfected += n;
+			n = n*r;
+            
+
+            day++;
+        }
+        System.out.println(day - 1);
+    }
+}
